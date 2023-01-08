30e45df4e556bd9054b77ae316b344637d56b80f
golamrabbiazad <golamrabbiazad@gmail.com>
2023-01-08 19:43:59 +0600

    fix:logout not works onclick

--- features/auth/api/logout.ts
+++ features/auth/api/logout.ts
@@ -1,6 +1,7 @@
+import { useMutation } from '@tanstack/react-query'
+
 import { apiClient } from '@lib/apiClient'
 import { queryClient } from '@lib/reactQuery'
-import { useMutation } from '@tanstack/react-query'
 
 export const logout = () => {
   return apiClient.post('/auth/logout')
